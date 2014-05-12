bb7be8cbd80dc382d0681015c278ab12aa3c967a
Greg McCabe <gsmccabe1@gmail.com>
2014-05-12 13:53:21 -0500

    High Low revised

--- High_Low.php
+++ High_Low.php
@@ -2,17 +2,21 @@
 
 $number = rand(1, 100);
 
+$number_of_guess = 0;
+
 fwrite(STDOUT, "Guess a number between 1 and 100?\n ");
 
 while ($number != $Guess){
 	$Guess = fgets(STDIN);
+
 	if ($Guess > $number) {
 		echo "Lower\n";	
 	} elseif ($Guess < $number) {
 		echo "Higher\n";
-	} else {
-		echo "Correct!\n";
 	}
+	$number_of_guess += 1;
 }
+
+echo "Correct! It took you $number_of_guess guesses!!\n";
 		
 ?>
