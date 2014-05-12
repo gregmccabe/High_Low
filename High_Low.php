f9a1b4b8d63d417497d9351d72f8ebada7c218ec
Greg McCabe <gsmccabe1@gmail.com>
2014-05-12 16:02:13 -0500

    High Low with argc/argv

--- High_Low.php
+++ High_Low.php
@@ -1,21 +1,34 @@
+
 <?php
 
-$number = rand(1, 100);
+if ($argc == 3) {
+	$number = mt_rand($argv[1], $argv[2]);
+	fwrite(STDOUT, "Guess a number between {$argv[1]} and {$argv[2]}\n ");
+} else {
+	
+
+	fwrite(STDOUT, "Enter a low value\n ");	
+	$low = trim(fgets(STDIN));
+	fwrite(STDOUT, "Enter a high value\n");	
+	$high = trim(fgets(STDIN));
+
+	$number = mt_rand($low, $high);
+	
+	fwrite(STDOUT, "Guess a number between $low and $high\n ");
+}
 
 $number_of_guess = 0;
 
-fwrite(STDOUT, "Guess a number between 1 and 100?\n ");
 
-while ($number != $Guess){
-	$Guess = fgets(STDIN);
-
+do {	
+$Guess = fgets(STDIN);
 	if ($Guess > $number) {
 		echo "Lower\n";	
 	} elseif ($Guess < $number) {
 		echo "Higher\n";
 	}
 	$number_of_guess += 1;
-}
+} while($number != $Guess);
 
 echo "Correct! It took you $number_of_guess guesses!!\n";
 		
